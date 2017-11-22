dce27b4624e539ba24378c7a7efdc553e318799c
hnarita <h.narita@opt.ne.jp>
2017-11-22 22:33:08 +0900

    Add GeoSample

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -2,27 +2,32 @@
 
 using Android.App;
 using Android.Content;
-using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace OptGeoMediator.Droid
+using Xamarin.Forms.Platform.Android;
+using Android.Content.PM;
+
+
+namespace OptGeoMediator.Android
 {
-    [Activity(Label = "OptGeoMediator.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+
+    [Activity(Label = "OptGeoMediator.Android.Android", Icon = "@drawable/icon", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity :
+    global::Xamarin.Forms.Platform.Android.FormsApplicationActivity // superclass new in 1.3
     {
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.FormsMaps.Init(this, bundle);
 
-            LoadApplication(new App());
+            LoadApplication(new App()); // method is new in 1.3
         }
     }
+
 }
--- OptGeoMediator/App.xaml.cs
+++ OptGeoMediator/App.xaml.cs
@@ -8,7 +8,22 @@ namespace OptGeoMediator
         {
             InitializeComponent();
 
-            MainPage = new OptGeoMediatorPage();
+            var tabs = new TabbedPage();
+
+            // demonstrates the map control with zooming and map-types
+            tabs.Children.Add(new MapPage { Title = "Map/Zoom", Icon = "glyphish_74_location.png" });
+
+            // demonstrates the map control with zooming and map-types
+            tabs.Children.Add(new PinPage { Title = "Pins", Icon = "glyphish_07_map_marker.png" });
+
+            // demonstrates the Geocoder class
+            tabs.Children.Add(new GeocoderPage { Title = "Geocode", Icon = "glyphish_13_target.png" });
+
+            // opens the platform's native Map app
+            tabs.Children.Add(new MapAppPage { Title = "Map App", Icon = "glyphish_103_map.png" });
+
+            MainPage = tabs;
+
         }
 
         protected override void OnStart()
